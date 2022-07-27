67b25ac24c91aae5d9f8f3523afd6fe983f39e57
woowahan <nosod3@naver.com>
2022-07-27 19:50:46 +0900

    [UI] #9 - 내역 등록 Fragment 생성 및 UI 배치

---
+++ app/src/main/java/com/psw9999/android_accountbook_18/ui/historyinput/HistoryInputFragment.kt
@@ -0,0 +1,18 @@
+package com.psw9999.android_accountbook_18.ui.historyinput
+
+import com.psw9999.android_accountbook_18.R
+import com.psw9999.android_accountbook_18.databinding.FragmentHistoryInputBinding
+import com.psw9999.android_accountbook_18.ui.common.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HistoryInputFragment :
+    BaseFragment<FragmentHistoryInputBinding>(R.layout.fragment_history_input) {
+
+    override fun initViews() {
+    }
+
+    override fun observe() {
+    }
+
+}
