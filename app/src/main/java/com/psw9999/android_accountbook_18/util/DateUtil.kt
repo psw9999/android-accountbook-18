7518d3311b630124ba4d08b045c6ad548ded0ead
woowahan <nosod3@naver.com>
2022-07-27 22:44:56 +0900

    [ADD] #12 - 오늘 날짜를 읽을 Calendar 인스턴스와 dateFormat 추가

---
+++ app/src/main/java/com/psw9999/android_accountbook_18/util/DateUtil.kt
@@ -0,0 +1,8 @@
+package com.psw9999.android_accountbook_18.util
+
+import java.util.*
+
+object DateUtil {
+    val currentDate: Calendar = Calendar.getInstance()
+    val dateFormat = "%d-%02d-%02d"
+}
