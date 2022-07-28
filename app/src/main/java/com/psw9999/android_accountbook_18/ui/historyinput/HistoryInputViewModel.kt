ebb569089a62209248f9a90aff79c297f116fb53
woowahan <nosod3@naver.com>
2022-07-28 13:21:48 +0900

    [FEAT] #12 - DB에서 읽어온 카테고리, 결제수단 스피너에 적용되도록 구현

--- app/src/main/java/com/psw9999/android_accountbook_18/ui/historyinput/HistoryInputViewModel.kt
+++ app/src/main/java/com/psw9999/android_accountbook_18/ui/historyinput/HistoryInputViewModel.kt
@@ -1,13 +1,12 @@
 package com.psw9999.android_accountbook_18.ui.historyinput
 
 import androidx.lifecycle.ViewModel
+import com.psw9999.android_accountbook_18.util.DateUtil.currentDate
 import com.psw9999.android_accountbook_18.util.DateUtil.dateFormat
 import kotlinx.coroutines.flow.MutableStateFlow
 import java.util.*
 
 class HistoryInputViewModel : ViewModel() {
-
-    val currentDate: Calendar = Calendar.getInstance()
 
     private val _historyDate = MutableStateFlow(
         String.format(
@@ -17,8 +16,22 @@ class HistoryInputViewModel : ViewModel() {
             currentDate.get(Calendar.DAY_OF_MONTH)))
     val historyDate = _historyDate
 
+    private val _paymentMethod = MutableStateFlow("")
+    val paymentMethod = _paymentMethod
+
+    private val _catergory = MutableStateFlow("")
+    val catergory = _catergory
+
     fun setHistoryDate(year: Int, month: Int, dayOfMonth: Int) {
         historyDate.value = String.format(dateFormat, year, month + 1, dayOfMonth)
+    }
+
+    fun setPaymentMethod(method : String) {
+        _paymentMethod.value = method
+    }
+
+    fun setCategory(category : String) {
+        _catergory.value = category
     }
 
 }
