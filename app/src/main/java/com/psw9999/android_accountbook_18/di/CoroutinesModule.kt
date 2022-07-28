c2ff0291dce3c0b2d71123137d1c2bb60da55bf5
woowahan <nosod3@naver.com>
2022-07-28 13:10:03 +0900

    [FEAT] #6 - CoroutineModule 생성

---
+++ app/src/main/java/com/psw9999/android_accountbook_18/di/CoroutinesModule.kt
@@ -0,0 +1,18 @@
+package com.psw9999.android_accountbook_18.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object CoroutinesModule {
+
+    @Provides
+    @Singleton
+    fun providesIODispatcher(): CoroutineDispatcher = Dispatchers.IO
+}
