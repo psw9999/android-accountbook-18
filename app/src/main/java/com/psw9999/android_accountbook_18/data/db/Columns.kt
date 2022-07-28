111280bf724a21976419e2613aec60d514355376
woowahan <nosod3@naver.com>
2022-07-28 12:43:59 +0900

    [REFACTOR] #5 - Columns Enum class 수정

--- app/src/main/java/com/psw9999/android_accountbook_18/data/db/Columns.kt
+++ app/src/main/java/com/psw9999/android_accountbook_18/data/db/Columns.kt
@@ -1,13 +1,22 @@
 package com.psw9999.android_accountbook_18.data.db
 
-enum class HistoryColumns {
-    id, amount, time, content, payment_id, category_id
+enum class HistoryColumns(val columnName : String) {
+    id("id"),
+    amount("amount"),
+    time("time"),
+    content("content"),
+    payment_id("payment_id"),
+    category_id("category_id")
 }
 
-enum class PaymentColumns {
-    id, method
+enum class PaymentColumns(val columnName : String) {
+    id("id"),
+    method("method")
 }
 
-enum class CategoryColumns {
-    id, isSpend, title, color
+enum class CategoryColumns(val columnName : String) {
+    id("id"),
+    is_spend("is_spend"),
+    title("title"),
+    color("color")
 }
