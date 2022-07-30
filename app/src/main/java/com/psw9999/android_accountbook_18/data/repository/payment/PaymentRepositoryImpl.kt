dacb6eed6d8eb9d7c0d9d4832bc155a7711f75ab
woowahan <nosod3@naver.com>
2022-07-30 17:12:32 +0900

    [FEAT] #17 - PaymentRepository savePayment, updatePayment 추가

--- app/src/main/java/com/psw9999/android_accountbook_18/data/repository/payment/PaymentRepositoryImpl.kt
+++ app/src/main/java/com/psw9999/android_accountbook_18/data/repository/payment/PaymentRepositoryImpl.kt
@@ -2,7 +2,6 @@ package com.psw9999.android_accountbook_18.data.repository.payment
 
 import com.psw9999.android_accountbook_18.data.Result
 import com.psw9999.android_accountbook_18.data.dto.PaymentDto
-import com.psw9999.android_accountbook_18.data.repository.payment.PaymentRepository
 import com.psw9999.android_accountbook_18.data.source.local.payment.PaymentDataSource
 import kotlinx.coroutines.CoroutineDispatcher
 import javax.inject.Inject
@@ -12,7 +11,14 @@ class PaymentRepositoryImpl @Inject constructor(
     private val ioDispatcher: CoroutineDispatcher
 ) : PaymentRepository {
 
-    override suspend fun getAllPayments(): Result<List<PaymentDto>>
-        = PaymentDataSource.getAllPayments()
+    override suspend fun getAllPayments(): Result<List<PaymentDto>> =
+        PaymentDataSource.getAllPayments()
 
+    override suspend fun savePayment(title: String) {
+        PaymentDataSource.savePayment(title)
+    }
+
+    override suspend fun updatePayment(title: String) {
+        PaymentDataSource.updatePayment(title)
+    }
 }
