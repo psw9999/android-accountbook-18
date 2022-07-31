7d8291075a7a92233500789b9bed7bf9463928ad
woowahan <nosod3@naver.com>
2022-08-01 00:46:01 +0900

    [FEAT] #27 - HistoryList CollectLastest 적용하여 리스트어댑터에 submit하도록 적용

--- app/src/main/java/com/psw9999/android_accountbook_18/ui/history/HistoryFragment.kt
+++ app/src/main/java/com/psw9999/android_accountbook_18/ui/history/HistoryFragment.kt
@@ -1,60 +1,56 @@
 package com.psw9999.android_accountbook_18.ui.history
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.psw9999.android_accountbook_18.R
+import com.psw9999.android_accountbook_18.databinding.FragmentHistoryBinding
+import com.psw9999.android_accountbook_18.ui.common.BaseFragment
+import com.psw9999.android_accountbook_18.ui.history.adapter.HistoryListAdapter
+import com.psw9999.android_accountbook_18.ui.historyinput.HistoryInputFragment
+import com.psw9999.android_accountbook_18.ui.main.HistoryViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+@AndroidEntryPoint
+class HistoryFragment : BaseFragment<FragmentHistoryBinding>(R.layout.fragment_history){
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HistoryFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class HistoryFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private val historyViewModel : HistoryViewModel by viewModels()
+    private val historyListAdapter : HistoryListAdapter by lazy {HistoryListAdapter()}
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun observe() {
+        getHistoryList()
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                historyViewModel.historys.collectLatest {
+                    historyListAdapter.submitList(it)
+                }
+            }
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history, container, false)
+    override fun initViews() {
+        binding.viewModel = historyViewModel
+        binding.rvHistory.adapter = historyListAdapter
+        // TODO : 프래그먼트 매니저 구조 수정 예정
+        binding.fbtnHistoryAdd.setOnClickListener {
+            val transaction = activity?.supportFragmentManager?.beginTransaction()?.add(R.id.l_main_container, HistoryInputFragment())
+            transaction!!.addToBackStack(null)
+            transaction.hide(this)
+            transaction.commit()
+        }
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HistoryFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HistoryFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun getHistoryList() {
+        historyViewModel
+        CoroutineScope(Dispatchers.IO).launch {
+            historyViewModel.getMonthHistorys(
+                historyViewModel.selectedDate.value.year, historyViewModel.selectedDate.value.monthValue
+            )
+        }
     }
 }
