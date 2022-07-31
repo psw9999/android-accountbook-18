040422cb28d77db294760fc5409c8c0b56020878
woowahan <nosod3@naver.com>
2022-07-31 17:33:47 +0900

    [FEAT] #23 - HistoryRepository GetMonthHistory 외 update, delete 추가

--- app/src/main/java/com/psw9999/android_accountbook_18/data/repository/history/HistoryRepository.kt
+++ app/src/main/java/com/psw9999/android_accountbook_18/data/repository/history/HistoryRepository.kt
@@ -1,16 +1,28 @@
 package com.psw9999.android_accountbook_18.data.repository.history
 
 import com.psw9999.android_accountbook_18.data.Result
-import com.psw9999.android_accountbook_18.data.dto.HistoryDto
+import com.psw9999.android_accountbook_18.data.vo.HistoryVo
 
 interface HistoryRepository {
 
+    suspend fun getMonthHistorys(year : Int, month : Int): Result<List<HistoryVo>>
+
     suspend fun saveHistory(
-        time: String,
-        amount: Int,
-        content: String,
-        paymentId: Int?,
-        categoryId: Int
+        time : String,
+        amount : Int,
+        content : String,
+        paymentId : Int?,
+        categoryId : Int
     )
+
+    suspend fun updateHistory(
+        time : String,
+        amount : Int,
+        content : String,
+        paymentId : Int?,
+        categoryId : Int
+    )
+
+    suspend fun deleteHistories(idList: List<Int>)
 
 }
