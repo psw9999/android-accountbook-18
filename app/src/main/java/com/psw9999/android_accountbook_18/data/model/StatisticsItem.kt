e83e0aa781cc202dcef736ac06a239eab5c42a90
woowahan <nosod3@naver.com>
2022-08-02 22:58:32 +0900

    [FEAT] #35 - Statistics Item 추가 및 StatisticsListAdapter 구현

---
+++ app/src/main/java/com/psw9999/android_accountbook_18/data/model/StatisticsItem.kt
@@ -0,0 +1,8 @@
+package com.psw9999.android_accountbook_18.data.model
+
+data class StatisticsItem(
+    val category : String,
+    val categoryColor : Int,
+    val spendAmount : Int,
+    val percent : Int
+)
