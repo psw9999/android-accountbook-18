066da0e52014e2cf953e15c749c9d0be598ea016
woowahan <nosod3@naver.com>
2022-08-04 01:01:28 +0900

    [FEAT] #39 - PaymentDto, CategoryDto 데이터 클래스 Parcelable 적용

--- app/src/main/java/com/psw9999/android_accountbook_18/data/dto/CategoryDto.kt
+++ app/src/main/java/com/psw9999/android_accountbook_18/data/dto/CategoryDto.kt
@@ -1,8 +1,12 @@
 package com.psw9999.android_accountbook_18.data.dto
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class CategoryDto(
-    val id : Int,
-    var isSpend : Boolean,
-    var name : String,
-    var color : Int
-)
+    val id: Int,
+    var isSpend: Boolean,
+    var name: String,
+    var color: Int
+) : Parcelable
--- app/src/main/java/com/psw9999/android_accountbook_18/data/dto/PaymentDto.kt
+++ app/src/main/java/com/psw9999/android_accountbook_18/data/dto/PaymentDto.kt
@@ -1,6 +1,10 @@
 package com.psw9999.android_accountbook_18.data.dto
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class PaymentDto(
-    val id : Int,
-    var method : String
-)
+    val id: Int,
+    var method: String
+) : Parcelable
