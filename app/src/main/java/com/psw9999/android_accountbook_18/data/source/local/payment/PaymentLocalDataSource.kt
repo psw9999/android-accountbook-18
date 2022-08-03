fdd69a8ce4b0d225b3b0ca531d49e8e781fab94f
woowahan <nosod3@naver.com>
2022-08-04 01:08:14 +0900

    [FEAT] #39 - Payment Update 구현 및 Save, Update시 Result 반환

--- app/src/main/java/com/psw9999/android_accountbook_18/data/source/local/payment/PaymentLocalDataSource.kt
+++ app/src/main/java/com/psw9999/android_accountbook_18/data/source/local/payment/PaymentLocalDataSource.kt
@@ -1,16 +1,15 @@
 package com.psw9999.android_accountbook_18.data.source.local.payment
 
 import android.content.ContentValues
-import com.psw9999.android_accountbook_18.data.db.DatabaseHelper
-import com.psw9999.android_accountbook_18.data.dto.PaymentDto
 import com.psw9999.android_accountbook_18.data.Result
-import com.psw9999.android_accountbook_18.data.Result.Success
 import com.psw9999.android_accountbook_18.data.Result.Error
+import com.psw9999.android_accountbook_18.data.Result.Success
+import com.psw9999.android_accountbook_18.data.db.DatabaseHelper
 import com.psw9999.android_accountbook_18.data.db.DatabaseHelper.Companion.PAYMENT_TABLE
 import com.psw9999.android_accountbook_18.data.db.PaymentColumns
+import com.psw9999.android_accountbook_18.data.dto.PaymentDto
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
-import java.lang.Exception
 import javax.inject.Inject
 
 class PaymentLocalDataSource @Inject constructor(
@@ -39,17 +38,37 @@ class PaymentLocalDataSource @Inject constructor(
         }
     }
 
-    override suspend fun savePayment(title: String) {
-        withContext(ioDispatcher) {
+    override suspend fun savePayment(title: String): Result<Boolean> = withContext(ioDispatcher) {
+        return@withContext try {
             val wd = dataBaseHelper.writableDatabase
             val paymentValues = ContentValues().apply {
                 put(PaymentColumns.method.columnName, title)
             }
             wd.insert(PAYMENT_TABLE, null, paymentValues)
+            Success(true)
+        } catch (e: Exception) {
+            Error(e)
         }
     }
 
-    override suspend fun updatePayment(title: String) {
-        TODO("Not yet implemented")
-    }
+    override suspend fun updatePayment(paymentDto: PaymentDto): Result<Boolean> = withContext(ioDispatcher) {
+            return@withContext try {
+                val wd = dataBaseHelper.writableDatabase
+                val paymentValues = ContentValues().apply {
+                    put(PaymentColumns.id.columnName, paymentDto.id)
+                    put(PaymentColumns.method.columnName, paymentDto.method)
+                }
+                wd.update(
+                    PAYMENT_TABLE,
+                    paymentValues,
+                    "${PaymentColumns.id.columnName} = ${paymentDto.id}",
+                    null
+                )
+                Success(true)
+            } catch (e: Exception) {
+                Error(e)
+            }
+        }
 }
+
+
