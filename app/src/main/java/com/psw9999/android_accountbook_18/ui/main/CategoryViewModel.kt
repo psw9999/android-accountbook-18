859c72e2a7bf3cb63ede18d4177a2abf70e3b98e
woowahan <nosod3@naver.com>
2022-08-04 01:10:52 +0900

    [FEAT] #39 - Payment, Category의 ViewModel Result에 따른 로직 추가

--- app/src/main/java/com/psw9999/android_accountbook_18/ui/main/CategoryViewModel.kt
+++ app/src/main/java/com/psw9999/android_accountbook_18/ui/main/CategoryViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.viewModelScope
 import com.psw9999.android_accountbook_18.data.Result
 import com.psw9999.android_accountbook_18.data.dto.CategoryDto
 import com.psw9999.android_accountbook_18.data.repository.category.CategoryRepository
+import com.psw9999.android_accountbook_18.util.toast
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -16,26 +17,63 @@ class CategoryViewModel @Inject constructor(
     private val repository: CategoryRepository
 ) : ViewModel() {
 
-    // TODO : DTO -> Entity로 변경하여 적용
     private val _categorys = MutableStateFlow<List<CategoryDto>>(emptyList())
     val category : StateFlow<List<CategoryDto>> = _categorys
 
-    suspend fun getAllCategory() {
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading : StateFlow<Boolean> = _isLoading
+
+    private val _isComplete = MutableStateFlow(false)
+    val isComplete : StateFlow<Boolean> = _isComplete
+
+    fun setIsComplete(isComplete : Boolean) {
+        _isComplete.value = isComplete
+    }
+
+    fun getAllCategory() {
+        _isLoading.value = true
         viewModelScope.launch {
             repository.getAllCategorys().let { result ->
                 if (result is Result.Success) {
                     _categorys.value = result.data
                 } else {
-                    // TODO : DB Read 실패 UI에 보여주기
+                    toast("카테고리 로드에 실패하였습니다.")
                     _categorys.value = emptyList()
                 }
+                _isLoading.value = false
             }
         }
     }
 
-    suspend fun saveCategory(isSpend : Boolean, title : String, color : Int) {
+    fun saveCategory(isSpend : Boolean, title : String, color : Int) {
+        _isLoading.value = true
         viewModelScope.launch {
-            repository.saveCategory(isSpend, title, color)
+            repository.saveCategory(isSpend, title, color).let { result ->
+                if (result is Result.Success) {
+                    _isComplete.value = true
+                    toast("카테고리를 저장하였습니다.")
+                } else {
+                    toast("카테고리 저장에 실패하였습니다.")
+                    _categorys.value = emptyList()
+                }
+                _isLoading.value = false
+            }
+        }
+    }
+
+    fun updatePayment(categoryDto: CategoryDto) {
+        _isLoading.value = true
+        viewModelScope.launch {
+            repository.updateCategory(categoryDto).let { result ->
+                if (result is Result.Success) {
+                    _isComplete.value = true
+                    toast("카테고리를 수정하였습니다.")
+                } else {
+                    toast("카테고리 수정에 실패하였습니다.")
+                    _categorys.value = emptyList()
+                }
+                _isLoading.value = false
+            }
         }
     }
 
--- app/src/main/java/com/psw9999/android_accountbook_18/ui/main/PaymentViewModel.kt
+++ app/src/main/java/com/psw9999/android_accountbook_18/ui/main/PaymentViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.viewModelScope
 import com.psw9999.android_accountbook_18.data.Result
 import com.psw9999.android_accountbook_18.data.dto.PaymentDto
 import com.psw9999.android_accountbook_18.data.repository.payment.PaymentRepository
+import com.psw9999.android_accountbook_18.util.toast
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -20,23 +21,61 @@ class PaymentViewModel @Inject constructor(
     private val _payments = MutableStateFlow<List<PaymentDto>>(emptyList())
     val payments : StateFlow<List<PaymentDto>> = _payments
 
-    suspend fun getAllPayments() {
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading : StateFlow<Boolean> = _isLoading
+
+    private val _isComplete = MutableStateFlow(false)
+    val isComplete : StateFlow<Boolean> = _isComplete
+
+    fun setIsComplete(isComplete : Boolean) {
+        _isComplete.value = isComplete
+    }
+
+    fun getAllPayments() {
+        _isLoading.value = true
         viewModelScope.launch {
             repository.getAllPayments().let { result ->
                 if (result is Result.Success) {
                     _payments.value = result.data
                 } else {
-                    // TODO : DB Read 실패처리
-                    Log.e("DatabaseError","$result")
+                    toast("결제수단 로드에 실패하였습니다.")
                     _payments.value = emptyList()
                 }
+                _isLoading.value = false
             }
         }
     }
 
-    suspend fun savePayment(title : String) {
+    fun savePayment(title : String) {
+        _isLoading.value = true
         viewModelScope.launch {
-            repository.savePayment(title)
+            repository.savePayment(title).let { result ->
+                if (result is Result.Success) {
+                    _isComplete.value = true
+                    toast("결제수단을 저장하였습니다.")
+                } else {
+                    toast("결제수단 저장에 실패하였습니다.")
+                    _payments.value = emptyList()
+                }
+                _isLoading.value = false
+            }
         }
     }
+
+    fun updatePayment(paymentDto : PaymentDto) {
+        _isLoading.value = true
+        viewModelScope.launch {
+            repository.updatePayment(paymentDto).let { result ->
+                if (result is Result.Success) {
+                    _isComplete.value = true
+                    toast("결제수단을 수정하였습니다.")
+                } else {
+                    toast("결제수단 수정에 실패하였습니다.")
+                    _payments.value = emptyList()
+                }
+                _isLoading.value = false
+            }
+        }
+    }
+
 }
